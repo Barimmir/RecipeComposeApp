2836b7ac20c0e048012bc265e9f97c29955e7d1a
GrivnoKoder <grivnoisinsaideme@gmail.com>
2026-01-26 19:24:19 +0300

    RCA-04-Заменил тему приложения

--- app/src/main/java/com/example/recipecomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipecomposeapp/MainActivity.kt
@@ -5,12 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.recipecomposeapp.ui.theme.RecipeComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +15,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MaterialTheme {
+            RecipeComposeAppTheme {
                 Scaffold { paddingValues ->
                     Text(
                         "Recipe App",
@@ -27,21 +24,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RecipeComposeAppTheme {
-        Greeting("Android")
     }
 }
